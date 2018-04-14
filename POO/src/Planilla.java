73c329aeaf3c283126b5e2963aaeb285a61a7377
AchmedCL <30802613+AchmedCL@users.noreply.github.com>
2018-04-14 20:28:30 -0300

    Update Planilla.java

--- POO/src/Planilla.java
+++ POO/src/Planilla.java
@@ -1,15 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zakaz;
 
-/**
- *
- * @author Rurikk
- */
 public class Planilla
 {
-    
+    private Date fechaInicio;
+	private Date fechaFin;
+	private int codigo;
+	private Vehiculo transporte;
+	private Persona conductor;
+	private ArrayList<Encargo> encargos;
+	private Local despacho;
+	
+	public Planilla(int codigo)
+	{
+		this.codigo=codigo;
+		transporte=new Vehiculo();
+		encargos=new ArrayList<Encargo>();
+		conductor=new Persona();
+		despacho=new Local();
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 }
