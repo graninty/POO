16f3536a01a4e6a5e8f59f50f822bc4f09e77fc2
AchmedCL <30802613+AchmedCL@users.noreply.github.com>
2018-04-14 20:27:23 -0300

    Update ListaPlanillas.java

--- POO/src/ListaPlanillas.java
+++ POO/src/ListaPlanillas.java
@@ -1,15 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zakaz;
 
-/**
- *
- * @author Rurikk
- */
 public class ListaPlanillas
 {
-    
+    private ArrayList<Planilla> planillas;
+	
+	public ListaPlanillas()
+	{
+		planillas=new ArrayList<Planilla>();
+	}
+	public Planilla existePlanilla(int codigo)
+	{
+		for(int i=0;i<planillas.size();i++)
+		{
+			if((planillas.get(i).getCodigo())==codigo)
+				return planillas.get(i);
+		}
+		return null;
+	}
+	public boolean agregarPlanilla(int codigo)
+	{
+		if(existePlanilla(codigo)==null)
+		{
+			Planilla newPlanilla=new Planilla(codigo);
+			planillas.add(newPlanilla);
+			if(existePlanilla(codigo)!=null)
+				return true;
+		}
+		return false;
+	}
+	public boolean eliminarPlanilla(int codigo)
+	{
+		for(int i=0;i<planillas.size();i++)
+		{
+			if(existePlanilla(codigo)!=null)
+			{
+				if((planillas.get(i).getCodigo())==codigo)
+					planillas.remove(i);
+				if(existePlanilla(codigo)==null)
+					return true;
+			}
+		}
+		return false;
+	}
 }
