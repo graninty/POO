9077914c0b7b3429e0bfd6f290aabde4d16c8914
AchmedCL <30802613+AchmedCL@users.noreply.github.com>
2018-04-15 01:07:57 -0300

    Update ListaPlanillas.java

--- POO/src/ListaPlanillas.java
+++ POO/src/ListaPlanillas.java
@@ -42,4 +42,74 @@ public class ListaPlanillas
 		}
 		return false;
 	}
+	public boolean modificarFechaInicio(int codigo,Date fechaInicio) 
+	{
+		if(!planillas.isEmpty())
+		{
+			for(int i=0;i<planillas.size();i++)
+			{
+				if(buscarPlanilla(codigo)!=null)
+				{
+					planillas.get(i).setFechaInicio(fechaInicio);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public boolean modificarFechaFin(int codigo,Date fechaFin)
+	{
+		if(!planillas.isEmpty())
+		{
+			for(int i=0;i<planillas.size();i++)
+			{
+				planillas.get(i).setFechaFin(fechaFin);
+				return true;
+			}
+		}
+		return false;
+	}
+	public Vehiculo vehiculoMasUtilizado()
+	{
+		int mayor=0;
+		String mayorVehiculo = null;
+		Vehiculo vehiculos;
+		for(int i=0;i<planillas.size();i++)
+		{
+			vehiculos=((Vehiculo)(planillas.get(i).getTransporte()));
+			if(contarVehiculo(vehiculos.getPatente())>mayor)
+			{
+				mayorVehiculo=vehiculos.getPatente();
+			}
+		}
+		if(mayorVehiculo!=null)
+			return buscarVehiculo(mayorVehiculo);
+		else
+			return null;
+	}
+	public Vehiculo buscarVehiculo(String patente)
+	{
+		Vehiculo vehiculos;
+		for(int i=0;i<planillas.size();i++)
+		{
+			vehiculos=((Vehiculo)(planillas.get(i).getTransporte()));
+			if(vehiculos.getPatente()==patente)
+			{
+				return vehiculos;
+			}
+		}
+		return null;
+	}
+	public int contarVehiculo(String patenteVehi)
+	{
+		int cont=0;
+		for(int i=0;i<planillas.size();i++)
+		{
+			if(((Vehiculo)(planillas.get(i).getTransporte())).getPatente()==patenteVehi)
+			{
+				cont++;
+			}
+		}
+		return cont;
+	}
 }
